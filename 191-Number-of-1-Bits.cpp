40808361593b61b49b03408a96c97364f1f62686
Saatvika Singh <126290980+7veekaa@users.noreply.github.com>
2025-02-17 21:44:23 +0530

    [191] [Time Beats: 100.00%] [Memory Beats: 11.92%] - LeetPush

--- 191-Number-of-1-Bits.cpp
+++ 191-Number-of-1-Bits.cpp
@@ -4,10 +4,25 @@ public:
         int count=0;
         while(n){
             int bit=n&1;
-            if(bit) count++;
+            if(bit==1) count++; 
             n=n>>1;
         }
         return count;
         
     }
 };
+
+
+//Optimal way is some brian something algo, brian kernighan-
+/*class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n) {
+            count++;  
+            n = n & (n - 1); 
+        }
+        return count;
+    }
+};
+*/
